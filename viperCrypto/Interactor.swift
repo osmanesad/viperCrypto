cacb61f6ade2324127fe21b65f5a7b0844e6cd28
osmanesad <byesad@gmail.com>
2023-01-05 04:09:21 +0300

    Created Viper Files
    Viper için gerekli ortamı hazırladık. Temel dosyları oluşturduk. storyboard alakalı dosyalarını kaldırdık. Info.plist ayarını yaptık.

---
+++ viperCrypto/Interactor.swift
@@ -0,0 +1,8 @@
+//
+//  Interactor.swift
+//  viperCrypto
+//
+//  Created by Osman Esad on 5.01.2023.
+//
+
+import Foundation
---
+++ viperCrypto/View.swift
@@ -0,0 +1,9 @@
+//
+//  View.swift
+//  viperCrypto
+//
+//  Created by Osman Esad on 5.01.2023.
+//
+
+import Foundation
+
