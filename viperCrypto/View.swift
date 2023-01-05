a828e5e7c010ff938a8fd8e762926ba106b8fa9b
osmanesad <byesad@gmail.com>
2023-01-05 21:14:30 +0300

    Update - View
    View içini yazıyoruz.

--- viperCrypto/View.swift
+++ viperCrypto/View.swift
@@ -10,9 +10,48 @@ import UIKit
 
 protocol AnyView {
     
+    var presenter : AnyPresenter? {get set}
+    
+    func update(with cryptos: [Crypto])
+    func update(with error: String)
+    
 }
 
-class CryptoViewController : UIViewController {
+class CryptoViewController : UIViewController, AnyView, UITableViewDelegate, UITableViewDataSource {
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    var presenter: AnyPresenter?
+    
+    private let tableView: UITableView {
+        let table = UITableView()
+        table.register(UITableView.self, forCellReuseIdentifier: "cell")
+        table.isHidden = true
+        return table
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    override func viewDidLayoutSubviews() {
+        <#code#>
+    }
+    
+    func update(with cryptos: [Crypto]) {
+        
+    }
+    
+    func update (with error: String) {
+        
+    }
 }
 
